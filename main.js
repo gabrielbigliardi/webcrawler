86a305789c26034c968b8c4411683076e461e361
gabrielbigliardi <gabrielbigliardi@gmail.com>
2023-10-02 14:48:29 -0300

    report

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-const normalizeURL = require('./crawl')
+const { crawlPage } = require('./crawl.js')
 
-console.log('hello world')
-// normalizeURL('https://blog.boot.dev/path/')
+async function main(){
+  if (process.argv.length < 3){
+    console.log('no website provided')
+  }
+  if (process.argv.length > 3){
+    console.log('too many arguments provided')
+  }
+
+  const baseURL = process.argv[2]
+
+  console.log(`starting crawl of: ${baseURL}...`)
+
+  const pages = await crawlPage(baseURL, baseURL, {})
+
+  console.log(pages)
+}
+
+main()
+
